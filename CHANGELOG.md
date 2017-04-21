344ac5ec57a6a517ac16f88442c0085148bf6ee7
Corey Hemminger <corey.hemminger@nativex.com>
2017-04-21 15:10:04 -0500

    fixed typo
    Signed-off-by: Corey Hemminger <corey.hemminger@nativex.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,4 @@
-# CHANGELOG for timezone_ii
-
-## 2.0.0 (4/19/2017)
-
-- [Corey Hemminger] - changed hash rockets to new hash style
-
-## 1.0.1 (4/18/2017)
-
-- [Corey Hemminger] - fixed windows idempotency
+# CHANGELOG for artifactory_ii
 
 ## 1.0.0 (4/18/2017)
 
--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,17 @@ maintainer_email 'hemminger@hotmail.com'
 license 'Apache-2.0'
 description 'Installs/Configures artifactory_ii'
 long_description 'Installs/Configures artifactory_ii'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/artifactory_ii/issues'
+issues_url 'https://github.com/Stromweld/artifactory_ii/issues'
 
 # The `source_url` points to the development reposiory for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/artifactory_ii'
+source_url 'https://github.com/Stromweld/artifactory_ii'
